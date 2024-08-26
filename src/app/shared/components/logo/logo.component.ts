e88ddeca547f4749f7505d07b8e2e55e35fd1142
Eddy-glitcher <luis-gonzales6@hotmail.com>
2024-08-25 19:09:33 -0500

    Feat: Add the logo component

---
+++ src/app/shared/components/logo/logo.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.scss'
+})
+export class LogoComponent {
+
+  @Input() logoWidth?: string;
+  @Input() logoHeight?: string;
+  @Input() textSize?: string;
+
+}
