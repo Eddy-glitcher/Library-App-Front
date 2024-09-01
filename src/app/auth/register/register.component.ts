b6a6c48d02a788c2deacc313eb0bbe2eefffe7fd
Eddy-glitcher <luis-gonzales6@hotmail.com>
2024-09-01 10:54:46 -0500

    Style: Add the register form styles

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '../../shared/components/logo/logo.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ LogoComponent ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
+
 export class RegisterComponent {
 
 }
