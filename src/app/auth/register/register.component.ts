a9ff0320509bda2c99310ba07248c71e36a23b6f
Eddy-glitcher <luis-gonzales6@hotmail.com>
2024-09-01 20:30:26 -0500

    Feat: Add the show password button

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, Renderer2 } from '@angular/core';
 import { LogoComponent } from '../../shared/components/logo/logo.component';
 
 @Component({
@@ -10,5 +10,15 @@ import { LogoComponent } from '../../shared/components/logo/logo.component';
 })
 
 export class RegisterComponent {
+
+  private _renderer = inject(Renderer2);
+  private _elementRef = inject(ElementRef);
+  private _showPassword!: boolean;
+
+  showPassword(id : string): void{
+    this._showPassword = !this._showPassword;
+    const element = this._elementRef.nativeElement.querySelector(`#${id}`);
+    this._showPassword ? this._renderer.setProperty(element, 'type', 'text') : this._renderer.setProperty(element, 'type', 'password');
+  }
 
 }
