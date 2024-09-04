0d65b73d959f82247a4ad83dace8d811e43ede99
Eddy-glitcher <luis-gonzales6@hotmail.com>
2024-09-03 20:02:24 -0500

    Feat: Add the form group

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,10 @@
 import { Component, ElementRef, inject, Renderer2 } from '@angular/core';
 import { LogoComponent } from '../../shared/components/logo/logo.component';
-
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ LogoComponent ],
+  imports: [ LogoComponent, ReactiveFormsModule ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
@@ -14,6 +14,15 @@ export class RegisterComponent {
   private _renderer = inject(Renderer2);
   private _elementRef = inject(ElementRef);
   private _showPassword!: boolean;
+
+  private _formBuilder = inject(FormBuilder);
+
+  registerForm = this._formBuilder.group({
+    name     : ['', Validators.required],
+    email    : ['', Validators.required],
+    password : ['', [Validators.required, Validators.minLength(8)]],
+    confirm_password : ['', Validators.required]
+  });
 
   showPassword(id : string): void{
     this._showPassword = !this._showPassword;
