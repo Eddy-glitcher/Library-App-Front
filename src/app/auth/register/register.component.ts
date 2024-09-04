9b87b3028338d89ab2f4c832550ad6bc2e3d4a9a
Eddy-glitcher <luis-gonzales6@hotmail.com>
2024-09-03 21:54:11 -0500

    Feat: Add the form validation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, inject, Renderer2 } from '@angular/core';
 import { LogoComponent } from '../../shared/components/logo/logo.component';
-import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators} from '@angular/forms';
+import { FormValidatorService } from '../../shared/services/form-validator.service';
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -11,23 +12,42 @@ import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 
 export class RegisterComponent {
 
-  private _renderer = inject(Renderer2);
+  private _renderer   = inject(Renderer2);
   private _elementRef = inject(ElementRef);
   private _showPassword!: boolean;
 
-  private _formBuilder = inject(FormBuilder);
+  private _formBuilder      = inject(FormBuilder);
+  private _formValidatorSvc = inject(FormValidatorService);
 
-  registerForm = this._formBuilder.group({
+  registerForm: FormGroup = this._formBuilder.group({
     name     : ['', Validators.required],
-    email    : ['', Validators.required],
+    email    : ['', [Validators.required, Validators.pattern(this._formValidatorSvc.validEmail)]],
     password : ['', [Validators.required, Validators.minLength(8)]],
     confirm_password : ['', Validators.required]
+  },{
+    validators : this._formValidatorSvc.formEqualPasswordsValidator('password', 'confirm_password')
   });
 
   showPassword(id : string): void{
     this._showPassword = !this._showPassword;
     const element = this._elementRef.nativeElement.querySelector(`#${id}`);
     this._showPassword ? this._renderer.setProperty(element, 'type', 'text') : this._renderer.setProperty(element, 'type', 'password');
+  }
+
+  formValidator(control : string): boolean | ValidationErrors | null {
+    return this.registerForm.controls[control]?.touched && this.registerForm.controls[control].invalid;
+  }
+
+  formFieldValidator(control : string): string | void{
+    const controlErrors = this.registerForm.get(control)?.errors;
+
+    if(controlErrors?.['required']) return 'Este campo es requerido';
+
+    if (controlErrors?.['minlength']) return `Este debe tener almenos ${controlErrors?.['minlength'].requiredLength} caracteres`;
+
+    if (controlErrors?.['pattern']) return `Formato de correo inválido`;
+
+    if (controlErrors?.['passwordIncorrect']) return `Las contraseñas deben cohinsidir`;
   }
 
 }
