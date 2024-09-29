47c612e98252486609b117f54acd23f3aae2f7e7
Eddy-glitcher <luis-gonzales6@hotmail.com>
2024-09-28 21:22:17 -0500

    Feat: Add login route

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,6 +1,8 @@
+import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 
 export const  AuthRoutes = [
   { path : 'register', component : RegisterComponent },
+  { path : 'login', component : LoginComponent },
   { path : '**', redirectTo : 'register'}
 ];
