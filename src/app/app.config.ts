f73536fee9dd6f8f3f01e100573d005f4ec5e63e
Eddy-glitcher <luis-gonzales6@hotmail.com>
2024-10-01 18:48:48 -0500

    Feat: Add the htt provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withHttpTransferCacheOptions({includePostRequests : true}))]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withHttpTransferCacheOptions({includePostRequests : true})),
+    provideHttpClient()
+  ]
 };
