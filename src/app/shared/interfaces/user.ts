03fd65479aae8373dbb5db7d9933ce6b5e8532aa
Eddy-glitcher <luis-gonzales6@hotmail.com>
2024-10-01 18:58:15 -0500

    Feat: Add the user interface

---
+++ src/app/shared/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  cloudinary_resource : string;
+  email : string;
+  favorites : string[];
+  image : string;
+  isActive : boolean;
+  name : string;
+  role : string;
+  _id : string;
+}
