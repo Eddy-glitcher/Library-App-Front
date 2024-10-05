90e112903f591009733ee9c7c33315caae803642
Eddy-glitcher <luis-gonzales6@hotmail.com>
2024-10-04 19:33:08 -0500

    Feat: Improve the HttpClient configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withHttpTransferCacheOptions({includePostRequests : true})),
-    provideHttpClient()
+    provideHttpClient(withFetch())
   ]
 };
